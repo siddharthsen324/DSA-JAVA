28764e9e718e3a718bd6a23133ce2efbab8ca60f
Siddharth Sen <siddharthsen324@gmail.com>
2026-04-16 19:13:48 +0530

    PrintingNumber

---
+++ BASIC_JAVA/PrintingNumber.java
@@ -0,0 +1,7 @@
+// This program demonstrates the difference between printing a mathematical expression and printing it as a string.
+public class PrintingNumber {
+    static void main(String[] args) {
+        System.out.println(69+1);
+        System.out.println("69+1");
+    }
+}
