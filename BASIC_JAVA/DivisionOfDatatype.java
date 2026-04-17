c5ee7228869988c7b6f2364dd861669b3bad417d
Siddharth Sen <siddharthsen324@gmail.com>
2026-04-17 20:58:32 +0530

    DivisionOfDatatype

---
+++ BASIC_JAVA/DivisionOfDatatype.java
@@ -0,0 +1,12 @@
+
+//5/2=2.5 but java me 5/2=2 aayega because java me dono operand int hai to ans bhi int aayega
+// this is called integer division. To get the correct answer we can make one of the operand double by adding .0 to it. For example, 5.0/2 or 5/2.0 will give us 2.5 as the result.
+public class DivisionOfDatatype {
+   
+    static void main(String[] args) {
+        double x =5/2;//5.0 use karo correct ans aayega
+
+        System.out.println(x);
+    }
+    
+}
