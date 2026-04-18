71c0704c993e3433cfcaa68d8c525bddcd7b2122
Siddharth Sen <siddharthsen324@gmail.com>
2026-04-18 17:30:13 +0530

    variableBasic

---
+++ BASIC_JAVA/variableBasic.java
@@ -0,0 +1,23 @@
+// this is a java program to understand the concept of variable in java
+// variable is a container that holds the value. It is used to store data in java.
+public class variableBasic {
+    static void main(String[] args) {
+//        int x=5;
+//        System.out.println(x+" ");
+//        x=10;
+//        System.out.println(x);
+//
+//        int y;
+//        y=20;
+//        System.out.println(y);
+//        System.out.println(x*y);
+        int x=5;
+        System.out.println(x);
+        x=x+5;
+        System.out.println(x);
+        x=x+20;
+        System.out.println(x);
+
+    }
+    
+}
