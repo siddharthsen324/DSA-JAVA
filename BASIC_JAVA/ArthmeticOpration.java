462fbb64b2612ad5116b374c22d81e9018b32d67
Siddharth Sen <siddharthsen324@gmail.com>
2026-04-20 20:59:04 +0530

    ArthmeticOperations

---
+++ BASIC_JAVA/ArthmeticOpration.java
@@ -0,0 +1,14 @@
+// this is a simple java program to perform arthmetic operations
+public class ArthmeticOpration {
+    public static void main(String[] args) {
+//        int x=5;
+//        int y=2;
+        double x=39;
+        double y=10;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        // this is perform a many type of arthmetic operation in java. We can use +, -, *, / for addition, subtraction, multiplication and division respectively.
+    }
+}
