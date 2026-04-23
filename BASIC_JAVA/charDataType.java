346e87abcfdaac4213dc44d4d770772a4342b672
Siddharth Sen <siddharthsen324@gmail.com>
2026-04-23 20:49:42 +0530

    charDataType

---
+++ BASIC_JAVA/charDataType.java
@@ -0,0 +1,34 @@
+//this is a program to understand char data type in java
+public class charDataType {
+    static void main(String[] args) {
+//        char siddhu ='a';
+//        System.out.println(siddhu);
+//        char sidd = 's';
+//        System.out.println(sidd);
+
+//        char ch ='A';
+//        int x=ch;//implicit typecasting
+//        System.out.println(x);
+//
+//        char sid ='a';
+//        int y=(int)sid;
+//        System.out.println(y);
+//
+//        char sen ='3';
+//        System.out.println((int)sen);
+
+//        char ch ='b';
+//        System.out.println(ch+0);
+
+        //integer to charactor
+        int x=99;
+        char ch= (char)x;
+        System.out.println(ch);
+        // this all code explain about char data type in java. char data type is used to store single character in java.
+        // It is a 16-bit Unicode character. We can use char data type to store any character like alphabets, digits, special characters etc.
+        //  We can also perform typecasting to convert char to int and int to char.
+
+    }
+}
+
+
