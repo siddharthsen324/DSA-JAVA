1ee9f13e7c61a017013e88e5c48505ebc6aff22b
Siddharth Sen <siddharthsen324@gmail.com>
2026-04-24 20:14:04 +0530

    Increment Decrement operators

---
+++ BASIC_JAVA/IncrementDecrement.java
@@ -0,0 +1,18 @@
+// Increment and Decrement Operators in Java
+public class IncrementDecrement {
+    static void main(String[] args) {
+//        int sid =10;
+//         this is post increment operator. it will first print the value of sid and then it will increment the value of sid by 1.
+//        System.out.println(sid++);
+//         this will print the incremented value of sid which is 11. 
+//        System.out.println(sid);  
+//          this is pre increment operator. it will first increment the value of sid by 1 and then it will print the value of sid which is 12.
+//        System.out.println(++sid);
+//        System.out.println(sid);
+            int x=10;
+            int y=x++;//y=10
+        System.out.println(x+" "+y);
+    }
+}
+
+
