929192f8493b49fdb5c37eb5a9c233b0cb853d48
Siddharth Sen <siddharthsen324@gmail.com>
2026-04-26 13:21:05 +0530

    Variable Naming

---
+++ BASIC_JAVA/VariableNaming.java
@@ -0,0 +1,14 @@
+// this is a class for variable naming convention
+public class VariableNaming {
+    
+    public static void main(String[] args) {
+       // _ or alphabet or $ form started
+        // !@#$...... speacial
+        //not use speacial key word
+
+        int sumit=100;
+        int guddu=99;
+        System.out.println(sumit+guddu);
+
+    }
+}
