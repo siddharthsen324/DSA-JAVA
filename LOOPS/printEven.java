78a3baa0e2b0259a9610811da09ad226cca5a1c6
Siddharth Sen <siddharthsen324@gmail.com>
2026-05-02 21:48:03 +0530

    print Even

---
+++ LOOPS/printEven.java
@@ -0,0 +1,13 @@
+   public class printEven {
+   
+    
+   
+   static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+//            if(i%2!=0 && i%3==0) System.out.println(i); home work hai
+            if(i%2==0) System.out.println(i);
+
+        }
+
+        }
+    }
