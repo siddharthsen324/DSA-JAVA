99520af2a1e8958df37adababb3cf82fc3324165
Siddharth Sen <siddharthsen324@gmail.com>
2026-05-07 21:05:33 +0530

    Basic Loop

---
+++ LOOPS/BasicLoop.java
@@ -0,0 +1,10 @@
+public class BasicLoop {
+    static void main(String[] args) {
+        int i;
+        for (i =1 ; i <= 8; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        System.out.println(i);
+    }
+}
