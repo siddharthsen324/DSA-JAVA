45d71374ddd37626121f290b80e5655571459e70
Siddharth Sen <siddharthsen324@gmail.com>
2026-05-15 20:36:18 +0530

    Ap Series

---
+++ LOOPS/ApSeries.java
@@ -0,0 +1,20 @@
+public class ApSeries {
+        static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        for (int i = 2; i<=(3*n-1) ; i+=3) {
+//            System.out.print(i+" ");
+//    }
+
+
+//        int a=4,b=6;
+//        for (int i = 1; i <=n ; i++) {
+//            System.out.print(a+" ");
+//            a+=b;
+//
+//        }
+        for (int i = 99; i >0 ; i-=4) {
+            System.out.print(i+" ");
+        }
+    }
+}
