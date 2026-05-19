f111c41e918ccf1eb610071e52c002db65132c4f
Siddharth Sen <siddharthsen324@gmail.com>
2026-05-19 20:49:02 +0530

    a Ki Poweb B

---
+++ LOOPS/aKiPowebB.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class aKiPowebB {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b = sc.nextInt();
+        int p=1;
+        for(int i=1;i<=b;i++){
+            p*=a;
+        }
+        System.out.println(a+" raised to the power "+b+" is "+p);
+    }
+}
