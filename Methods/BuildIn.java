54a06f8f79ba246145f601193ea9f2a55b5fb3e9
Siddharth Sen <siddharthsen324@gmail.com>
2026-06-28 10:39:52 +0530

    BuildIn

---
+++ Methods/BuildIn.java
@@ -0,0 +1,13 @@
+public class BuildIn {
+    static void main(String[] args) {
+//        System.out.println(Math.sqrt(100));
+//        System.out.println(Math.cbrt(100));
+//        System.out.println(Math.abs(-45));
+//        System.out.println(Math.floor(-7.8));
+//        System.out.println(Math.ceil(3.0001));
+//        System.out.println(Math.min(3,9));
+//        System.out.println(Math.max(3,9));
+        System.out.println(Math.pow(2,6));
+
+    }
+}
