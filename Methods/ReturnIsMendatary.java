3a22a2f15d94e71a4b1bb5814f554c3414daebe8
Siddharth Sen <siddharthsen324@gmail.com>
2026-06-30 05:23:36 +0530

    ReturnIsMendatary

---
+++ Methods/ReturnIsMendatary.java
@@ -0,0 +1,11 @@
+public class ReturnIsMendatary {
+    static void main(String[] args) {
+        System.out.println(sidd(3));
+    }
+    public static int sidd(int a){
+        if(a<5){
+            return a;
+        }
+        return -3;
+    }
+}
