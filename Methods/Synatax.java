2bfb06bfcc844e73ce627239be42240b1a96b042
Siddharth Sen <siddharthsen324@gmail.com>
2026-09-13 15:28:30 +0530

    Synatax

---
+++ Methods/Synatax.java
@@ -0,0 +1,18 @@
+public class Synatax {
+    public static void main(String[] args) {
+        siddhu();
+    }
+    public static void siddhu(){
+        System.out.println("hello siddhu");
+        manu();
+    }
+    public static void manu(){
+
+        System.out.println("hello manu");
+        himu();
+    }
+    public static void himu(){
+        System.out.println("hello himu");
+//        siddhu();
+    }
+}
