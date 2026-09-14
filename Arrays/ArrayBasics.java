389498d3dbd515ba8ebc60c951798e192186aae5
Siddharth Sen <siddharthsen324@gmail.com>
2026-09-14 11:37:19 +0530

    Array Basics

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,14 @@
+public class ArrayBasics {
+    static void main(String[] args) {
+//        int[] x={1,2,3,4,5,6};
+//        System.out.println(x[3]);
+//        x[3]=89;
+//        System.out.println(x[3]);
+        int[] arr=new int[4];
+        arr[0]=5;
+        arr[1]=5;
+        arr[2]=5;
+        arr[3]=5;
+        System.out.println(arr);
+    }
+}
